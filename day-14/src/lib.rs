139b455d1360632fd8df2197f2719914d5106d29
David Rueck <drueck@gmail.com>
2022-12-19 22:26:10 -0800

    Day 14, Part 2

--- day-14/src/lib.rs
+++ day-14/src/lib.rs
@@ -1,10 +1,12 @@
 use std::collections::HashMap;
 use std::ops::RangeInclusive;
 
+#[derive(Clone)]
 pub struct Cave {
     pub map: HashMap<(usize, usize), char>,
     pub deepest_rocks: HashMap<usize, usize>,
-    pub sand_at_rest: usize,
+    pub floor: Option<usize>,
+    pub grains_at_rest: usize,
 }
 
 impl Cave {
@@ -48,37 +50,55 @@ impl Cave {
         Self {
             map,
             deepest_rocks,
-            sand_at_rest: 0,
+            grains_at_rest: 0,
+            floor: None,
         }
     }
 
-    // drop a grain of sand and return the position at which it rests, or None if it falls forever
+    pub fn build_floor(&mut self) {
+        self.floor = Some(self.deepest_rocks.values().max().unwrap() + 2)
+    }
+
+    // drop a grain of sand and return the position at which it rests,
+    // or None if it falls forever or if the cave is totally full
     pub fn drop_sand(&mut self, x: usize, y: usize) -> Option<(usize, usize)> {
-        // falling forever
-        match self.deepest_rocks.get(&x) {
-            None => return None,
-            Some(deepest_y) => {
-                if y >= *deepest_y {
-                    return None;
+        // cave is full
+        if self.map.get(&(x, y)).is_some() {
+            return None;
+        }
+
+        // falling infinitely
+        if self.floor.is_none() {
+            match self.deepest_rocks.get(&x) {
+                None => return None,
+                Some(deepest_y) => {
+                    if y >= *deepest_y {
+                        return None;
+                    }
                 }
             }
         }
 
-        // down
-        if self.map.get(&(x, y + 1)).is_none() {
+        if self.is_open(x, y + 1) {
             self.drop_sand(x, y + 1)
-        // down and left
-        } else if self.map.get(&(x - 1, y + 1)).is_none() {
+        } else if self.is_open(x - 1, y + 1) {
             self.drop_sand(x - 1, y + 1)
-        // down and right
-        } else if self.map.get(&(x + 1, y + 1)).is_none() {
+        } else if self.is_open(x + 1, y + 1) {
             self.drop_sand(x + 1, y + 1)
-        // can't move, so add it to the map
         } else {
-            self.sand_at_rest += 1;
+            self.grains_at_rest += 1;
             self.map.insert((x, y), 'o');
             Some((x, y))
         }
+    }
+
+    pub fn is_open(&self, x: usize, y: usize) -> bool {
+        let at_the_floor = match self.floor {
+            Some(floor) => y == floor,
+            None => false,
+        };
+
+        !at_the_floor && self.map.get(&(x, y)).is_none()
     }
 }
 
@@ -95,21 +115,33 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_drop_sand() {
+    fn test_drop_sand_no_floor() {
         let input = std::fs::read_to_string("test-input.txt").unwrap();
         let mut cave = Cave::new(&input);
         cave.drop_sand(500, 0);
 
-        assert_eq!(cave.sand_at_rest, 1);
+        assert_eq!(cave.grains_at_rest, 1);
         assert_eq!(cave.map.get(&(500, 8)), Some(&'o'));
     }
 
     #[test]
-    fn drop_till_you_stop() {
+    fn part_1() {
         let input = std::fs::read_to_string("test-input.txt").unwrap();
         let mut cave = Cave::new(&input);
 
         while cave.drop_sand(500, 0).is_some() {}
-        assert_eq!(cave.sand_at_rest, 24);
+        assert_eq!(cave.grains_at_rest, 24);
+    }
+
+    #[test]
+    fn part_2() {
+        let input = std::fs::read_to_string("test-input.txt").unwrap();
+        let mut cave_with_floor = Cave::new(&input);
+
+        cave_with_floor.build_floor();
+        assert_eq!(cave_with_floor.floor, Some(11));
+
+        while cave_with_floor.drop_sand(500, 0) != Some((500, 0)) {}
+        assert_eq!(cave_with_floor.grains_at_rest, 93);
     }
 }
--- day-14/src/main.rs
+++ day-14/src/main.rs
@@ -11,9 +11,18 @@ fn main() {
     let input = fs::read_to_string(&input_filename).expect("failed to read input file");
     let mut cave = Cave::new(&input);
 
+    let mut cave_with_floor = cave.clone();
+    cave_with_floor.build_floor();
+
     while cave.drop_sand(500, 0).is_some() {}
     println!(
-        "The number of grains that came to rest were {}",
-        cave.sand_at_rest
+        "The number of grains that came to rest in the cave with no floor were {}",
+        cave.grains_at_rest
+    );
+
+    while cave_with_floor.drop_sand(500, 0).is_some() {}
+    println!(
+        "The number of grains that came to rest in the cave with no floor were {}",
+        cave_with_floor.grains_at_rest
     );
 }
