0770d3b0e37d70df27cd518d8d9db80402db5ef9
David Rueck <drueck@gmail.com>
2022-12-27 23:12:34 -0800

    Day 16, Part 2: Works for test input, but too many states for real input

--- day-16/src/main.rs
+++ day-16/src/main.rs
@@ -2,10 +2,13 @@
 // https://adventofcode.com/2022/day/16
 // Usage: `cargo run <input-file>`
 
+use itertools::iproduct;
 use regex::Regex;
 use std::collections::{HashMap, HashSet, VecDeque};
 use std::env;
 use std::fs;
+use Action::{Open, Visit, Wait};
+use Strategy::{Alone, WithElephant};
 
 const TIME_LIMIT: usize = 30;
 
@@ -15,14 +18,24 @@ fn main() {
     let valves = parse_input(&input);
 
     println!(
-        "The max pressure released after 30 seconds is: {}",
-        part_1(&valves)
+        "The max pressure released with you working alone is: {}",
+        find_best_pressure(&valves, Alone)
+    );
+
+    println!(
+        "The max pressure released with you and the elephant working together is: {}",
+        find_best_pressure(&valves, WithElephant)
     );
 }
 
-fn part_1(valves: &HashMap<&str, Valve>) -> usize {
+pub enum Strategy {
+    Alone,
+    WithElephant,
+}
+
+fn find_best_pressure(valves: &HashMap<&str, Valve>, strategy: Strategy) -> usize {
     let mut best_total_pressures: HashMap<String, usize> = HashMap::new();
-    let mut queue: VecDeque<State> = VecDeque::new();
+    let mut queue: VecDeque<GlobalState> = VecDeque::new();
 
     let starting_valve = valves.get(&"AA").unwrap();
     let valves_worth_opening = valves
@@ -30,16 +43,24 @@ fn part_1(valves: &HashMap<&str, Valve>) -> usize {
         .filter(|(_, valve)| valve.flow_rate > 0)
         .count();
 
-    queue.push_back(State::new(
-        starting_valve.id,
+    let (human_state, elephant_state) = match strategy {
+        Alone => (WorkerState::new(starting_valve.id, 0), None),
+        WithElephant => (
+            WorkerState::new(starting_valve.id, 4),
+            Some(WorkerState::new(starting_valve.id, 4)),
+        ),
+    };
+
+    queue.push_back(GlobalState::new(
+        human_state,
+        elephant_state,
         HashSet::new(),
         HashSet::new(),
-        0,
         0,
     ));
 
-    while let Some(state) = queue.pop_front() {
-        for next_state in next_states(&valves, &state) {
+    while let Some(global_state) = queue.pop_front() {
+        for next_state in next_global_states(&valves, &global_state) {
             let best_pressure = best_total_pressures
                 .entry(next_state.hash_key())
                 .or_insert(0);
@@ -56,34 +77,32 @@ fn part_1(valves: &HashMap<&str, Valve>) -> usize {
     best_total_pressures.into_values().max().unwrap()
 }
 
-// closed valves that you can reach under the time limit from the current state
-pub fn next_states<'a>(valves: &HashMap<&str, Valve<'a>>, state: &State<'a>) -> Vec<State<'a>> {
-    // if we can't navigate to an adjacent valve and open it by minute 29
-    // then there are no viable next states from this state
-    if state.minutes_elapsed > TIME_LIMIT - 3 {
+pub fn next_moves<'a>(
+    worker_state: &WorkerState<'a>,
+    global_state: &GlobalState<'a>,
+    valves: &HashMap<&str, Valve<'a>>,
+) -> Vec<Move<'a>> {
+    if worker_state.minutes_elapsed > TIME_LIMIT - 3 {
         return vec![];
     }
 
-    let mut next_states: Vec<State> = vec![];
+    let mut next_moves: Vec<Move> = vec![];
     // (valve_id, minutes elapsed to get to this valve, valves visited in path)
     let mut valves_to_check: VecDeque<(&'a str, usize, Vec<&'a str>)> = VecDeque::new();
-    let current_valve = valves.get(state.valve_id).unwrap();
+    let current_valve = valves.get(worker_state.valve_id).unwrap();
 
-    // the current valve is open (or we're choosing not to open it?) and the minutes elapsed is
-    // the time after it was opened
-    // now what we want to check is the adjacent valves, so we push those on the queue
-    // and the time for each is the time we would ARRIVE at that valve (not including any opening
-    // time)
     for id in &current_valve.next_valves {
-        valves_to_check.push_back((&id, state.minutes_elapsed + 1, vec![current_valve.id]));
+        valves_to_check.push_back((
+            &id,
+            worker_state.minutes_elapsed + 1,
+            vec![current_valve.id],
+        ));
     }
 
     while let Some((id, minutes, valves_in_path)) = valves_to_check.pop_front() {
         let valve = valves.get(id).unwrap();
-        if valve.flow_rate == 0 || state.open_valves.contains(&id) {
-            // this valve is open, so we want to check valves we can get to from
-            // here, and it will take 1 minute to go to the next valve
-            // and then it would take two minutes to open it, so skip ones we couldn't open
+        if valve.flow_rate == 0 || global_state.open_valves.contains(&id) {
+            // open or not worth opening
             if minutes + 2 < TIME_LIMIT {
                 for id in valve
                     .next_valves
@@ -96,80 +115,110 @@ pub fn next_states<'a>(valves: &HashMap<&str, Valve<'a>>, state: &State<'a>) -> 
                 }
             }
         } else {
-            // there are two options when we encounter a closed valve,
-            // we can open it, or we can keep going in search of better options
-            // let's push both options on to the next_states queue
-
-            // add the valves in the path to this node to the list of visited valves
-            let mut visited_valves = state.visited_valves.clone();
-            for valve_in_path in valves_in_path {
-                if !state.open_valves.contains(valve_in_path) {
-                    visited_valves.insert(valve_in_path);
-                }
-            }
-
-            // don't open the valve even though it's closed, just navigate to it
-            // we don't need to consider this unless we'd still be able to get to another
-            // closed valve and open it by minute 29
-            if minutes + 2 < TIME_LIMIT {
-                visited_valves.insert(id);
-                next_states.push(State::new(
-                    id,
-                    state.open_valves.clone(),
-                    visited_valves.clone(),
-                    state.total_pressure,
-                    minutes,
-                ));
-            }
-
-            // navigate to the valve and open it
-            // it takes one minute to open the valve, so only consider this if we have time
-            if minutes + 1 < TIME_LIMIT {
-                let mut open_valves = state.open_valves.clone();
-                open_valves.insert(id);
-
-                let minutes_elapsed = minutes + 1;
-
-                let total_pressure =
-                    state.total_pressure + valve.flow_rate * (TIME_LIMIT - minutes_elapsed);
-
-                next_states.push(State::new(
-                    id,
-                    open_valves,
-                    visited_valves,
-                    total_pressure,
-                    minutes_elapsed,
-                ))
-            }
+            // closed - we have two option here, so add both of them
+            // visit
+            next_moves.push(Move::new(id, valves_in_path.clone(), Visit, minutes, 0));
+            // open
+            next_moves.push(Move::new(
+                id,
+                valves_in_path,
+                Open,
+                minutes + 1,
+                valve.flow_rate * (TIME_LIMIT - minutes - 1),
+            ))
         }
     }
 
-    next_states
+    next_moves
 }
 
-#[derive(Debug)]
-pub struct State<'a> {
-    pub valve_id: &'a str,
+pub fn next_global_states<'a>(
+    valves: &HashMap<&str, Valve<'a>>,
+    global_state: &GlobalState<'a>,
+) -> Vec<GlobalState<'a>> {
+    let mut next_global_states = vec![];
+
+    let next_human_moves = next_moves(&global_state.human_state, &global_state, &valves);
+    let next_elephant_moves = match &global_state.elephant_state {
+        Some(elephant_state) => next_moves(&elephant_state, &global_state, &valves),
+        None => vec![Move::new(&"AA", vec![], Wait, 0, 0)],
+    };
+
+    for (human_move, elephant_move) in iproduct!(&next_human_moves, &next_elephant_moves) {
+        if human_move.opening_same_valve_as(&elephant_move) {
+            continue;
+        }
+        let mut next_global_state = (*global_state).clone();
+        next_global_state.apply_human_move(&human_move);
+        next_global_state.apply_elephant_move(&elephant_move);
+        next_global_states.push(next_global_state);
+    }
+
+    next_global_states
+}
+
+#[derive(Clone, Debug)]
+pub struct GlobalState<'a> {
+    pub human_state: WorkerState<'a>,
+    pub elephant_state: Option<WorkerState<'a>>,
     pub open_valves: HashSet<&'a str>,
     pub visited_valves: HashSet<&'a str>,
     pub total_pressure: usize,
-    pub minutes_elapsed: usize,
 }
 
-impl<'a> State<'a> {
+impl<'a> GlobalState<'a> {
     pub fn new(
-        valve_id: &'a str,
+        human_state: WorkerState<'a>,
+        elephant_state: Option<WorkerState<'a>>,
         open_valves: HashSet<&'a str>,
         visited_valves: HashSet<&'a str>,
         total_pressure: usize,
-        minutes_elapsed: usize,
     ) -> Self {
         Self {
-            valve_id,
+            human_state,
+            elephant_state,
             open_valves,
             visited_valves,
             total_pressure,
-            minutes_elapsed,
+        }
+    }
+
+    pub fn apply_human_move(&mut self, m: &Move<'a>) {
+        if m.action != Wait {
+            self.human_state = WorkerState::new(m.valve_id, m.minutes_elapsed);
+            self.apply_move(&m);
+        }
+    }
+
+    // just ignore any move if there's no existing elephant state
+    // need to improve the ergonomics of this
+    pub fn apply_elephant_move(&mut self, m: &Move<'a>) {
+        if m.action != Wait {
+            self.elephant_state = Some(WorkerState::new(m.valve_id, m.minutes_elapsed));
+            self.apply_move(&m);
+        }
+    }
+
+    fn apply_move(&mut self, m: &Move<'a>) {
+        match m.action {
+            Visit => {
+                self.visited_valves.insert(m.valve_id);
+                for v in &m.valves_in_path {
+                    if !self.open_valves.contains(v) {
+                        self.visited_valves.insert(v);
+                    }
+                }
+            }
+            Open => {
+                self.open_valves.insert(m.valve_id);
+                self.total_pressure += m.pressure_released;
+                for v in &m.valves_in_path {
+                    if !self.open_valves.contains(v) {
+                        self.visited_valves.insert(v);
+                    }
+                }
+            }
+            Wait => {}
         }
     }
 
@@ -179,6 +228,58 @@ impl<'a> State<'a> {
         open_valves.sort();
         visited_valves.sort();
         format!("{}-{}", open_valves.join(""), visited_valves.join(""))
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct WorkerState<'a> {
+    pub valve_id: &'a str,
+    pub minutes_elapsed: usize,
+}
+
+impl<'a> WorkerState<'a> {
+    pub fn new(valve_id: &'a str, minutes_elapsed: usize) -> Self {
+        Self {
+            valve_id,
+            minutes_elapsed,
+        }
+    }
+}
+
+#[derive(Eq, PartialEq)]
+pub enum Action {
+    Visit,
+    Open,
+    Wait,
+}
+
+pub struct Move<'a> {
+    pub valve_id: &'a str,
+    pub valves_in_path: Vec<&'a str>,
+    pub action: Action,
+    pub minutes_elapsed: usize,
+    pub pressure_released: usize,
+}
+
+impl<'a> Move<'a> {
+    pub fn new(
+        valve_id: &'a str,
+        valves_in_path: Vec<&'a str>,
+        action: Action,
+        minutes_elapsed: usize,
+        pressure_released: usize,
+    ) -> Self {
+        Self {
+            valve_id,
+            valves_in_path,
+            action,
+            minutes_elapsed,
+            pressure_released,
+        }
+    }
+
+    pub fn opening_same_valve_as(&self, other: &Self) -> bool {
+        self.valve_id == other.valve_id && self.action == Open && other.action == Open
     }
 }
 
@@ -243,6 +344,13 @@ mod tests {
     fn test_part_1() {
         let input = fs::read_to_string("test-input.txt").unwrap();
         let valves = parse_input(&input);
-        assert_eq!(part_1(&valves), 1651);
+        assert_eq!(find_best_pressure(&valves, Alone), 1651);
+    }
+
+    #[test]
+    fn test_part_2() {
+        let input = fs::read_to_string("test-input.txt").unwrap();
+        let valves = parse_input(&input);
+        assert_eq!(find_best_pressure(&valves, WithElephant), 1707);
     }
 }
